dccb3cb0cc3162d1359f0c976b8e8f6ef226022b
Adam Paz <timothy.paz@us.sogeti.com>
2016-12-08 14:38:46 -0600

    Basic Login page setup with modal login form popup wired up. Still need to create the login form and back button, etc.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { ModalController, ViewController } from 'ionic-angular';
+import { LoginPage } from  '../login/login'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //
+  // constructor(public navCtrl: NavController) {
+  //
+  // }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public modalCtrl: ModalController) {
 
+  }
+
+  showLoginForm() {
+    console.log('Show Login Form Clicked..');
+    let loginModal = this.modalCtrl.create(LoginPage);
+    loginModal.present();
   }
 
 }
---
+++ src/pages/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  templateUrl: 'login.html'
+})
+
+export class LoginPage {
+
+  constructor () {
+
+  }
+}
